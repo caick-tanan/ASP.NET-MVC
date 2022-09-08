da92cf3d7a7633afff96eb69427a458a912ef0f3
Caick Tanan <caick_tanan@hotmail.com>
2022-09-08 16:29:51 -0300

    Delete seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -37,5 +37,28 @@ namespace SalesWebMvc.Controllers
             _sellerService.Insert(seller); //inseriou a pessoa
             return RedirectToAction(nameof(Index)); 
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindyById(id.Value); //busquei no BD
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete (int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
