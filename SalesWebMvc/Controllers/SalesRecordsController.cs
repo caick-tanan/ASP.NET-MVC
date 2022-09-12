4be250883eecd20529bc36afee8c2856de265889
Caick Tanan <caick_tanan@hotmail.com>
2022-09-12 15:48:01 -0300

    Implementing simple search

--- SalesWebMvc/Controllers/SalesRecordsController.cs
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -1,20 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using SalesWebMvc.Services;
+
 
 namespace SalesWebMvc.Controllers
 {
     public class SalesRecordsController : Controller
     {
+
+        private readonly SalesRecordService _salesRecordService;
+
+        public SalesRecordsController(SalesRecordService salesRecordService)
+        {
+            _salesRecordService = salesRecordService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult SimpleSearch()
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1); //caso o usuário nao informe uma data mínima, retornará a partir do dia 1 de janeiro do ano em que ele estiver
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now; //vai retorna o valor máximo atual
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd"); //passando os dados na forma do viewData
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            var result = await _salesRecordService.FindByDateAsync(minDate, maxDate); //vai pegar o método FindByDate, com as datas mínimas e máximas
+            return View(result);
         }
         public IActionResult GroupingSearch()
         {
---
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SalesRecordService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj; //essa declaração pega o SalesRecord que é do tipo Dbset e contruir para mim um objeto result do tipo Iquarible
+            if (minDate.HasValue) //Caso o minDate possua valor
+            {
+                result = result.Where(x => x.Date >= minDate.Value); //vai pegar o objeto que foi contruido por meio do link e comparar com o minDate
+            }
+            if (maxDate.HasValue) //Caso o maxDate possua valor
+            {
+                result = result.Where(x => x.Date <= maxDate.Value); //vai pegar o objeto que foi contruido por meio do link e comparar com o minDate
+            }
+            return await result
+                .Include(x => x.Seller) //faz um join com a tabela de Seller
+                .Include(x => x.Seller.Departament) //faz um join com a tabela de Departament
+                .OrderByDescending(x => x.Date) //Ordem Decrescente
+                .ToListAsync(); //vai ordenar em formato de lista
+        }
+    }
+}
