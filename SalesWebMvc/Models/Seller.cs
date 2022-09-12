70ba5feb1d02d3b2c5ad2de08a3579c8c0e01165
Caick Tanan <caick_tanan@hotmail.com>
2022-09-12 11:05:15 -0300

    App locale, number and date formatting

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System; //para acessar o DateTime
 using System.Collections.Generic; //para acessar o ICollection
+using System.ComponentModel.DataAnnotations;
 using System.Linq; //para acessar o where
 
 namespace SalesWebMvc.Models
@@ -8,9 +9,19 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]//Gera um link para o email automaticamente
         public string Email { get; set; }
+
+        [Display(Name = "Birth Date")] //SERVE PARA MUDAR O NOME NA WEB E NÃO DEIXAR JUNTO O NOME IGUAL NA VARIÁVEL
+        [DataType(DataType.Date)] //Apenas ano, mês e dia
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")] //Define a data no formato dia, mês e ano
         public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString = "{0:F2}")] //Define duas casas decimais no valor 
         public double BaseSalary { get; set; }
+
         public Departament Departament { get; set; }
         public int DepartamentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -2,12 +2,15 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
 using SalesWebMvc.Data;
 using SalesWebMvc.Services;
+using System.Globalization;
+using System.Collections.Generic;
 
 namespace SalesWebMvc
 {
@@ -46,6 +49,17 @@ namespace SalesWebMvc
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+
+            var enUs = new CultureInfo("en-US"); //localidade padrão o Estados Unidos
+            var locazitaionOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUs),
+                SupportedCultures = new List<CultureInfo> {enUs},
+                SupportedUICultures = new List<CultureInfo> {enUs}
+            };
+
+            app.UseRequestLocalization(locazitaionOptions); 
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
